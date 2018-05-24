375a773f400579431cf2fb01cfe5e3cea1e4bd98
AvraamNikolaou <avnikolaou@hotmail.com>
2018-05-24 11:50:31 +0300

    added 'Name Defined/NOT empty' test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,6 +32,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+        it("have a URL defined and not empty", function () {
+            allFeeds.forEach(function (feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).toBeGreaterThan(0);
+            })
+        });
     });
 
 
