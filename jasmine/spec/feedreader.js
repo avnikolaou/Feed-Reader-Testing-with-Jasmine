1c6b2064e41f34ad27198f3ce38cfa259612273a
AvraamNikolaou <avnikolaou@hotmail.com>
2018-05-24 14:02:29 +0300

    Updated commnets

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,13 +1,3 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
     describe("RSS Feeds", function() {
         it("are defined", function() {
@@ -15,10 +5,7 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        /* Test that loops through each feed in the allFeeds object and ensures it has a URL defined and not empty. */
 
         it("have a URL defined and not empty", function () {
             allFeeds.forEach(function (feed) {
@@ -27,10 +14,7 @@ $(function() {
             })
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        /* Test that loops through each feed in the allFeeds object and ensures it has a name defined and not empty. */
 
         it("have a name defined and not empty", function () {
             allFeeds.forEach(function (feed) {
@@ -41,25 +25,17 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Test suite named "The menu" for menu functionality. */
 
     describe("The Menu", function () {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        /* Test that ensures the menu element is hidden by default. */
         
         it("menu element hidden by default", function () {
             expect($("body").hasClass("menu-hidden")).toBe(true);
         })
 
-        /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* Test that ensures the menu changes visibility when the menu icon is clicked. */
         
         it("menu changes visibility when the menu icon is clicked", function () {
             $(".menu-icon-link").click();
@@ -70,15 +46,13 @@ $(function() {
         })
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Test suite named "Initial Entries" for feed loading functionality. */
 
     describe("Initial Entries", function () {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
         let feedLength;
@@ -96,14 +70,11 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Test suite named "New Feed Selection" for the feed selection functionality. */
     
     describe("New Feed Selection", function () {
         
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+        /* Test that ensures when a new feed is loaded by the loadFeed function that the content actually changes. */
         let firstFeed, secondFeed;
         beforeEach(function (done) {
             loadFeed(0, function () {
